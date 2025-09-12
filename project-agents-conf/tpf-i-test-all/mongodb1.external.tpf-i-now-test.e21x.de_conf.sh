ec11f7f263c2a09ebca381a6431375a7793e9692
root <root@mobil.hessen.de>
2025-09-12 10:01:26 +0000

    Automated configuration update for project tpf-i-test-all at Fri Sep 12 10:01:26 AM UTC 2025

---
+++ project-agents-conf/tpf-i-test-all/mongodb1.external.tpf-i-now-test.e21x.de_conf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "--- Running Icinga Agent Setup for mongodb1.external.tpf-i-now-test.e21x.de ---"
+
+# Stop Icinga2 if running to prevent issues
+systemctl stop icinga2
+
+# Run the node setup command
+icinga2 node setup \
+--ticket cab6f1722a5b57f8a76b8104ba3539866dd862e4 \
+--cn mongodb1.external.tpf-i-now-test.e21x.de \
+--endpoint "deployment.external.tpf-i-noo-test.e21x.de,172.29.36.50" \
+--zone master \
+--parent_host "deployment.external.tpf-i-noo-test.e21x.de" \
+--trustedcert "/var/lib/icinga2/certs/trusted-parent.crt" \
+--accept-config \
+--accept-commands \
+--disable-confd
+
+if [ $? -eq 0 ]; then
+    echo "Node setup command completed successfully."
+else
+    echo "Node setup command failed. Please check the output above."
+    exit 1
+fi

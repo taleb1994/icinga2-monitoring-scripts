9959f123e019ab5f9c057c274d55fdf7f2c4664c
root <root@mobil.hessen.de>
2025-09-12 12:43:10 +0000

    Automated configuration update for project tpf-i-test-all at Fri Sep 12 12:43:10 PM UTC 2025

---
+++ generate_agents_configuration/project-agents-conf/tpf-i-test-all/deployment.external.tpf-i-ost-test.e21x.de_conf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "--- Running Icinga Agent Setup for deployment.external.tpf-i-ost-test.e21x.de ---"
+
+# Stop Icinga2 if running to prevent issues
+systemctl stop icinga2
+
+# Run the node setup command
+icinga2 node setup \
+--ticket a29239bd74b4fb1b89eed8025c8b45c60970955b \
+--listen 0.0.0.0,5665 \
+--cn deployment.external.tpf-i-ost-test.e21x.de \
+--zone deployment.external.tpf-i-ost-test.e21x.de \
+--endpoint deployment.external.tpf-i-noo-test.e21x.de,172.29.36.50,5665 \
+--parent_zone master \
+--parent_host deployment.external.tpf-i-noo-test.e21x.de \
+--trustedcert /var/lib/icinga2/certs/trusted-parent.crt \
+--accept-config \
+--accept-commands \
+--disable-confd
+
+if [ $? -eq 0 ]; then
+    echo "Node setup command completed successfully."
+else
+    echo "Node setup command failed. Please check the output above."
+    exit 1
+fi
---
+++ generate_agents_configuration/project-agents-conf/tpf-i-test-all/kafka1.external.tpf-i-noo-test.e21x.de_conf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "--- Running Icinga Agent Setup for kafka1.external.tpf-i-noo-test.e21x.de ---"
+
+# Stop Icinga2 if running to prevent issues
+systemctl stop icinga2
+
+# Run the node setup command
+icinga2 node setup \
+--ticket c32a26e338a8d768a5e56375b61d6a8d41444ae6 \
+--listen 0.0.0.0,5665 \
+--cn kafka1.external.tpf-i-noo-test.e21x.de \
+--zone kafka1.external.tpf-i-noo-test.e21x.de \
+--endpoint deployment.external.tpf-i-noo-test.e21x.de,172.29.36.50,5665 \
+--parent_zone master \
+--parent_host deployment.external.tpf-i-noo-test.e21x.de \
+--trustedcert /var/lib/icinga2/certs/trusted-parent.crt \
+--accept-config \
+--accept-commands \
+--disable-confd
+
+if [ $? -eq 0 ]; then
+    echo "Node setup command completed successfully."
+else
+    echo "Node setup command failed. Please check the output above."
+    exit 1
+fi
